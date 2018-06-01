8042f62b072cbd7abe4b0a6203f1742763383c70
Jeury Mejia <jeury.mejia@gmail.com>
2018-05-31 23:43:37 -0400

    Start working on EggTimer. Add buttons to StoryBoard

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Jeury M Mejia  on 5/31/18.
+//  Copyright © 2018 MejiaCorps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
